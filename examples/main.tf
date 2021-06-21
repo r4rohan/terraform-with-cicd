dd94bf4cb3e30357dffd93a49db03e6e82fedfc8
Rohan Singh <rohandash1998@gmail.com>
2021-06-21 19:50:50 +0530

    testing fmt

--- examples/main.tf
+++ examples/main.tf
@@ -11,8 +11,8 @@ locals {
 module "kylo_ren" {
   source           = "../modules/gce"
   suffix           = local.suffix
-  gcp_project_id   = var.gcp_project_id
-  vpc_network_name = "default"
+  gcp_project_id       =  var.gcp_project_id
+vpc_network_name = "default"
   instance_name    = "kylo-ren"
   network_tags     = ["http-server", "https-server", "ssh"]
 }
