1b06f687d1700d97e8e84d58e4dc3cf5ec2f442d
Rohan Singh <30438229+r4rohan@users.noreply.github.com>
2021-06-21 17:59:13 +0530

    Update tf_apply.sh

--- .github/workflows/tf_apply.sh
+++ .github/workflows/tf_apply.sh
@@ -1,13 +1,16 @@
 # exit script upon any non-zero exit code
 set -o errexit
 
-source /home/runner/work/terraform-with-cicd/terraform-with-cicd/.github/workflows/script.sh
+# source /home/runner/work/terraform-with-cicd/terraform-with-cicd/.github/workflows/script.sh
+
+terraform init
+terraform get
 
 # show terraform plan file
-terraform show tf_plan
+terraform show "tf_plan"
 
 # provision GCP resource
-terraform apply tf_plan
+terraform apply "tf_plan"
 
 # remove terraform plan file
 rm tf_plan
