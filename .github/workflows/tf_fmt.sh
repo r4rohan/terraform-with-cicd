c52417c950b4ab324856bda9b6a4ebe0a1312865
Rohan Singh <rohandash1998@gmail.com>
2021-06-21 19:32:23 +0530

    remove unnecessary code, add tf_fmt.sh and tf_init script

--- .github/workflows/tf_fmt.sh
+++ .github/workflows/tf_fmt.sh
@@ -1,9 +1,5 @@
 # exit script upon any non-zero exit code
 set -o errexit
 
-# initialization of terraform
-terraform init
-terraform get
-
 # format checking of terraform
-terraform fmt -recursive
+terraform fmt -recursive -check -diff
