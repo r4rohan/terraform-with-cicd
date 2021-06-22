c015eacdbd522c198eed5af5239856aa81e2ff10
Rohan Singh <rohandash1998@gmail.com>
2021-06-22 16:21:05 +0530

    update output referance name and add slack notification for feature branch push

--- modules/gce/outputs.tf
+++ modules/gce/outputs.tf
@@ -4,22 +4,22 @@ output "instance_name" {
 }
 
 output "id" {
-  value       = google_compute_instance.kylo_ren.id
+  value       = google_compute_instance.gce.id
   description = "An identifier for the resource with format projects/{{project}}/zones/{{zone}}/instances/{{name}}"
 }
 
 output "instance_id" {
-  value       = google_compute_instance.kylo_ren.instance_id
+  value       = google_compute_instance.gce.instance_id
   description = "The server-assigned unique identifier of this instance. Example: 4567719474035761998"
 }
 
 output "self_link" {
-  value       = google_compute_address.kylo_ren_static_ip.self_link
+  value       = google_compute_address.gce_static_ip.self_link
   description = "The URI of the External Static IP resource."
 }
 
 output "email" {
-  value       = google_service_account.kylo_ren_sa.email
+  value       = google_service_account.gce_sa.email
   description = "The URI of the Google Service Account resource."
 }
 
