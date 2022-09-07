2c179e5c0ce6d02daddac76af407fd6d1324de33
brettoster <oster.brett@gmail.com>
2022-09-07 15:57:00 -0500

    try this one out

--- modules/gcp/main.tf
+++ modules/gcp/main.tf
@@ -2,17 +2,17 @@ terraform {
   required_version = ">= 0.13.1" # see https://releases.hashicorp.com/terraform/
 }
 
-locals {
-  instance_name = format("%s-%s", var.instance_name, var.suffix)
-  region        = data.google_client_config.google_client.region
-  zone          = format("%s-%s", local.region, var.zone)
-  network_tags  = tolist(toset(var.network_tags))
-
-  name_static_vm_ip = format("%s-ext-ip-%s", var.instance_name, var.suffix)
-
-  sa_id = format("%s-sa-%s", var.instance_name, var.suffix)
-}
-
+#locals {
+#  instance_name = format("%s-%s", var.instance_name, var.suffix)
+#  region        = data.google_client_config.google_client.region
+#  zone          = format("%s-%s", local.region, var.zone)
+#  network_tags  = tolist(toset(var.network_tags))
+#
+#  name_static_vm_ip = format("%s-ext-ip-%s", var.instance_name, var.suffix)
+#
+#  sa_id = format("%s-sa-%s", var.instance_name, var.suffix)
+#}
+#
 
 resource "google_compute_network" "vpc_network" {
   count                   = var.active ? 1 : 0
