831195a951ecfec6f04e1313bbd41c2940bd7abe
brettoster <oster.brett@gmail.com>
2022-09-07 15:53:14 -0500

    try this one out

--- gcp1/main.tf
+++ gcp1/main.tf
@@ -3,8 +3,9 @@ locals {
 }
 
 module "gcp1" {
+  source = "../modules/gcp"
   #suffix           = local.suffix
-  source           = "../modules/gcp"
+  #source           = "../modules/gcp"
   #gcp_project_id   = var.gcp_project_id
   #vpc_network_name = "default"
   #instance_name    = "gcp1"
