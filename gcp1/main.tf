5495e3ae94ceb30bd8ac1744843a586d207df5db
brettoster <oster.brett@gmail.com>
2022-09-07 15:52:49 -0500

    try this one out

--- gcp1/main.tf
+++ gcp1/main.tf
@@ -5,9 +5,9 @@ locals {
 module "gcp1" {
   #suffix           = local.suffix
   source           = "../modules/gcp"
-  gcp_project_id   = var.gcp_project_id
-  vpc_network_name = "default"
-  instance_name    = "gcp1"
-  network_tags     = ["http-server", "https-server", "horse"]
+  #gcp_project_id   = var.gcp_project_id
+  #vpc_network_name = "default"
+  #instance_name    = "gcp1"
+  #network_tags     = ["http-server", "https-server", "horse"]
 }
 
