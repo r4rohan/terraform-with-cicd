4a469dc462fed447b728032b2b28e1dbb8f45bde
brettoster <oster.brett@gmail.com>
2022-09-09 16:09:46 -0500

    try this one out

--- gcp2/main.tf
+++ gcp2/main.tf
@@ -7,7 +7,7 @@ module "gcp" {
   active           = var.active
   source           = "../modules/gcp"
   gcp_project_id   = var.gcp_project_id
-  vpc_network_name = "${gcp_instance_id}-default"
+  vpc_network_name = "${var.gcp_instance_id}-default"
   instance_name    = var.gcp_instance_id
   network_tags     = var.tags
   #suffix           = local.suffix
