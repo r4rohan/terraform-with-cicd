0dbe61350464883c5a4cb58bfbeacd58d3df6d59
brettoster <oster.brett@gmail.com>
2022-09-11 18:21:54 -0500

    try this one out

--- modules/gce/script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-sudo google_metadata_script_runner --script-type startup
-sudo apt update -y; sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
-sudo apt install nginx -y
-sudo ufw allow 'Nginx HTTP'
