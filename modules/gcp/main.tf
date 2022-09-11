4e6052ad4a66f62eacb072e58fcd67d6a6a2ea72
brettoster <oster.brett@gmail.com>
2022-09-11 10:16:53 -0500

    try this one out

--- modules/gcp/main.tf
+++ modules/gcp/main.tf
@@ -5,7 +5,7 @@ terraform {
 resource "google_compute_network" "vpc_network" {
   count                   = var.active ? 1 : 0
   #name                    = "terraform-network"
-  name                    = "${var.gcp_instance_id}-network"
+  name                    = "${var.instance_name}-network"
   auto_create_subnetworks = "true"
 }
 
